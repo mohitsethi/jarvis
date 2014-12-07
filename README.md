dc485c331a82b9ebaf82c94336260c61aa518a4e
mohitsethi <mohit@sethis.in>
2014-12-07 08:06:41 +0530

    initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+# jarvis
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'jarvis'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures jarvis'
+long_description 'Installs/Configures jarvis'
+version          '0.1.0'
+
