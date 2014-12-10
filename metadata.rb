8514020765ddf0d4102fa3aef6dde57b2fda0765
mohitsethi <mohit@sethis.in>
2014-12-10 16:19:27 +0000

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,28 @@
 name             'jarvis'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
+maintainer       'Mohit Sethi'
+maintainer_email 'mohit@sethis.in'
+license          'Apache 2.0'
 description      'Installs/Configures jarvis'
 long_description 'Installs/Configures jarvis'
 version          '0.1.0'
 
+
+%w{ windows ubuntu fedora redhat centos osx }.each do |os|
+  supports os
+end
+
+{
+  'windows' => '>= 1.2.8',
+  'chocolatey' => '~> 0.1.0',
+  'apt' => '~> 2.5.3',
+  'yum' => '~> 3.2.2',
+  'git' => '~> 4.0.2',
+  'golang' => '~> 1.4.0',
+  'chef_handler' => '~> 1.1.6',
+  'vim' => '~> 1.1.2',
+  'vagrant' => '~> 0.2.0',
+  'python' => '~> 1.4.7',
+  'docker' => '~> 0.35.2'
+}.each_pair do |cb, ver|
+  depends cb, ver
+end
